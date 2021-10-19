a996b5387471a2b0688a6e79beaf01acd3098a02
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-10-19 11:54:17 +0200

    [ref] ProductController: GetMapping used

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,18 +1,15 @@
 package com.ecommerce.microcommerce.web.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
-    @RequestMapping(value = "/Products", method = RequestMethod.GET)
+    @GetMapping(value = "/Products")
     public String productList() {
         return "An example of product";
     }
 
-    @RequestMapping(value = "/Products/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/Products/{id}")
     public String displayAProduct(@PathVariable int id) {
         return "You have asked a product with the id " + id;
     }
