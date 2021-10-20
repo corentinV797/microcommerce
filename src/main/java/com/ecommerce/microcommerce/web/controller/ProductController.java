a3ae0c8d50c971c47e649f8fc4066c983c296a0e
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-10-20 12:29:48 +0200

    [feat] ProductController: addProduct POST request returns right code + location of new product

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.ecommerce.microcommerce.web.controller;
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,7 +25,16 @@ public class ProductController {
     }
 
     @PostMapping(value = "/Products")
-    public void addProduct(@RequestBody Product p) {
-        productDao.save(p);
+    public ResponseEntity<Void> addProduct(@RequestBody Product p) {
+        Product productToAdd = productDao.save(p);
+        if (productToAdd == null) {
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productToAdd.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
