e095daffd5e2e7b1a97bc6d2edef3c572b93551e
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-10-20 10:06:32 +0200

    [feat] ProductDaoImpl

---
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDaoImpl.java
@@ -0,0 +1,36 @@
+package com.ecommerce.microcommerce.dao;
+
+import com.ecommerce.microcommerce.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductDaoImpl implements ProductDao {
+    public static List<Product> productList = new ArrayList<Product>();
+    static {
+        productList.add(new Product(1, new String("vacuum cleaner"), 200));
+        productList.add(new Product(2, new String("dish washer"), 300));
+        productList.add(new Product(3, new String("fridge"), 550));
+    }
+    @Override
+    public List<Product> findAll() {
+        return productList;
+    }
+
+    @Override
+    public Product ProductfindById(int id) {
+        Product p = productList.stream()
+                .filter(product -> id == product.getId())
+                .findAny()
+                .orElse(null);
+        return p;
+    }
+
+    @Override
+    public Product Productsave(Product p) {
+        productList.add(p);
+        return p;
+    }
+}
