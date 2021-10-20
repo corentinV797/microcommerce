1cc594e2e8b287a9b6dc91fd38da13978b9a7a8a
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-10-20 11:37:31 +0200

    [feat] ProductController: ProductDao used

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,18 +1,23 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+    @Autowired
+    ProductDao productDao;
     @GetMapping(value = "/Products")
-    public String productList() {
-        return "An example of product";
+    public List<Product> productList() {
+        return productDao.findAll();
     }
 
     @GetMapping(value = "/Products/{id}")
     public Product displayAProduct(@PathVariable int id) {
-        Product p = new Product(id, "Fridge", 500);
-        return p;
+        return productDao.ProductfindById(id);
     }
 }
